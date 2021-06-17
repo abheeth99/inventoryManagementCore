78cd23ad0e190c1c31f10d72b4ce2c872a2448fe
abheeth99 <Abheethk@embla.asia>
2021-06-17 20:28:25 +0530

    remove GetAllInventoryLogs

--- inventoryManagementCore/inventoryManagementCore/Services/InventoryService/IInventoryService.cs
+++ inventoryManagementCore/inventoryManagementCore/Services/InventoryService/IInventoryService.cs
@@ -11,7 +11,6 @@ namespace inventoryManagementCore.Services.InventoryService
     public interface IInventoryService
     {
         Task<ServiceResponse<List<GetInventoryDto>>> GetAllInventories();
-        Task<ServiceResponse<List<InventoryLog>>> GetAllInventoryLogs();
         Task<ServiceResponse<GetInventoryDto>> GetInventoryById(int id);
         Task<ServiceResponse<List<GetInventoryDto>>> AddInventory(AddInventoryDto newInventory);
         Task<ServiceResponse<GetInventoryDto>> UpdateInventory(UpdateInventoryDto updatedInventory);
